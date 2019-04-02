4053a8e2280368139845d9c4a3b95aca3a439df8
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-02 13:58:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     if params[:operation] == "add"
       
-    elsif params[:operation]
+    elsif params[:operation] == "subtract"
       
     elsif params[:operation]
     

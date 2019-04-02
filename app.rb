50b36c38d4bf240e945637c142569170d1d6668a
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-02 13:36:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get 'reversename/:name' do 
+  get '/reversename/:name' do 
     @reversed_name = params[:name].reverse
     "#{@reversed_name}"
   end 

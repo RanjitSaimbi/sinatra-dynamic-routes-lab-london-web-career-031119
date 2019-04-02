d1623c2107b27ed596a2cbf759a4efe9c8aec71c
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-02 14:01:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     elsif params[:operation] == "multiply"
       "#{params[:number1].to_i * params[:number2].to_i}"
     elsif params[:operation] == "divide"
-      "#{}"
+      "#{params[:number1].to_i / params[:number2].to_i}"
     end 
       
   end 
